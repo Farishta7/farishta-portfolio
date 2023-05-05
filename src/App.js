03bbfc45c7cd89cf78523b0162ad943f730bace6
Farishta7 <farishtatq@gmail.com>
2023-05-05 13:38:49 +0100

    inserted components into routing

---
+++ src/App.js
@@ -0,0 +1,27 @@
+import './App.css';
+import Nav from './components/Nav';
+import {Route, Routes} from 'react-router-dom';
+import Home from './components/Home';
+import Projects from './components/Projects';
+import Education from './components/Education';
+import Skills from './components/Skills';
+import Interests from './components/Interests';
+
+
+function App() {
+  return (
+    <div className="App">
+      <Nav />
+      <Routes>
+        <Route path='/' element={<Home />}/>
+        <Route path='/projects' element={<Projects  />}/>
+        <Route path='/education' element={<Education  />}/>
+        <Route path='/skills' element={<Skills  />}/>
+        <Route path='/interests' element={<Interests />} />
+       
+      </Routes>
+    </div>
+  );
+}
+
+export default App;
