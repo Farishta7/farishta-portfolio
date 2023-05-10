b501401431fb21c6a39ffd84b44c7e4b3a4ece26
Farishta7 <farishtatq@gmail.com>
2023-05-10 15:14:33 +0100

    additional classnames

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -4,17 +4,31 @@ import Footer from './Footer'
 
 const Home = () => {
   return (
-      <>
+    <>
+    <div className='parent-div'>
       <div className="text-block">
         <p className="home-text">Welcome to my portfolio!</p>
         <p className="home-text">I am a Northcoders Software Engineering bootcamp graduate with a background in Accounting. After graduating in Accounting, I experimented with short coding courses through Code Academy before joining Northcoders and this decision is what sparked my interest to become a developer. With plenty of transferable skills from Accounting (e.g. critical thinking, organisation skills, technical skills) as well as an intensive 13 weeks of skills built up during the bootcamp, I am excited to start a career as a Software Developer 🚀 </p>
       </div>
-      <div className='footer-div-on-pages'>
-      <Footer />
-      </div>
       
+      </div>
+      <Footer />
       </>
+      
     )
   };
   
 export default Home;
+
+// (
+//   <div className='home-div'>
+//   <div className="text-block">
+//     <p className="home-text">Welcome to my portfolio!</p>
+//     <p className="home-text">I am a Northcoders Software Engineering bootcamp graduate with a background in Accounting. After graduating in Accounting, I experimented with short coding courses through Code Academy before joining Northcoders and this decision is what sparked my interest to become a developer. With plenty of transferable skills from Accounting (e.g. critical thinking, organisation skills, technical skills) as well as an intensive 13 weeks of skills built up during the bootcamp, I am excited to start a career as a Software Developer 🚀 </p>
+//   </div>
+//   <div className='footer-div-on-pages'>
+//   <Footer />
+//   </div>
+  
+//   </div>
+// )
