3eeaa6255bbe29b5f0ee8cd7195edb47d977fb17
Farishta7 <farishtatq@gmail.com>
2023-05-16 10:39:46 +0100

    Add className for styling

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -7,8 +7,8 @@ const Home = () => {
     <>
     <div className='parent-div'>
       <div className="text-block">
-        <p className="home-text">Welcome to my portfolio!</p>
-        <p className="home-text">I am a Northcoders Software Engineering bootcamp graduate with a background in Accounting. After graduating in Accounting, I experimented with short coding courses through Code Academy before joining Northcoders and this decision is what sparked my interest to become a developer. With plenty of transferable skills from Accounting (e.g. critical thinking, organisation skills, technical skills) as well as an intensive 13 weeks of skills built up during the bootcamp, I am excited to start a career as a Software Developer 🚀 </p>
+        <p >Welcome to my portfolio!</p>
+        <p >I am a Northcoders Software Engineering bootcamp graduate with a background in Accounting. After graduating in Accounting, I experimented with short coding courses through Code Academy before joining Northcoders and this decision is what sparked my interest to become a developer. With plenty of transferable skills from Accounting (e.g. critical thinking, organisation skills, technical skills) as well as an intensive 13 weeks of skills built up during the bootcamp, I am excited to start a career as a Software Developer 🚀 </p>
       </div>
       
       </div>
