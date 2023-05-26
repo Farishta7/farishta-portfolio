d47e62322a0e8f5a00555d400c6bdfa7bafc0ff2
Farishta7 <farishtatq@gmail.com>
2023-05-26 11:17:27 +0100

    removed interests tab

--- src/components/Interests.jsx
+++ src/components/Interests.jsx
@@ -1,13 +1,15 @@
 import Footer from './Footer'
 import '../App.css';
 
-
 const Interests = () => {
   return (
     <>
       <div>
       <h1>Interests</h1>
         <p>My interests</p>
+        <div style={{ width: '30%' }}>
+   
+    </div>
     </div>
       <div className='footer-div-on-pages'>
       <Footer />
