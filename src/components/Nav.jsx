e633e8d4c069376db7d2a7692feb2226562d51d4
Farishta7 <farishtatq@gmail.com>
2023-05-26 11:17:43 +0100

    removed some tabs from navbar

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -3,10 +3,9 @@ import logo from '../farishta-tariq-portfolio-title.png'
 
 const Nav = () => {
     return (
-        <nav className="nav">
-          <div style={{paddingBottom: '20px'}}>
-          
-          <Link  to="/">
+      <nav className="nav">
+        <div style={{ paddingBottom: '20px' }}>
+          <Link to="/">
           <img src={logo} alt='logo'style={{width: '45rem'}} className="title"/>
           </Link>
           </div>
@@ -14,9 +13,9 @@ const Nav = () => {
       <div className='nav-box'>
       <Link to="/" className='nav-tabs'>Home | </Link>
       <Link to="/projects" className='nav-tabs'>My Projects | </Link>
-      <Link to="/education" className='nav-tabs'>Education | </Link>
-      <Link to="/skills" className='nav-tabs'>Skills | </Link>
-      <Link to="/interests" className='nav-tabs'>Interests</Link>
+      <Link to="/education" className='nav-tabs'>Education</Link>
+      {/* <Link to="/skills" className='nav-tabs'>Skills | </Link> */}
+      {/* <Link to="/interests" className='nav-tabs'>Interests</Link> */}
       </div>
     </nav>
         
