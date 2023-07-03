b2323c98e91d208e899610ddeb750f53fb66559a
Farishta7 <farishtatq@gmail.com>
2023-07-03 16:07:26 +0100

    test - done

--- src/components/Education.jsx
+++ src/components/Education.jsx
@@ -4,7 +4,7 @@ import '../App.css';
 const Education = () => {
   return (
     <>
-      <h1 class='text-4xl ...'>Educationnnn</h1>
+      <h1 class='text-4xl ...'>Education</h1>
       <div className='parent-div'>
         <div className="text-block">
           
