d6fff68093c7d6986b0e5ab1dc1bb3f875ed7c92
Farishta7 <farishtatq@gmail.com>
2023-07-03 15:58:17 +0100

    updated

--- src/components/Skills.jsx
+++ src/components/Skills.jsx
@@ -3,16 +3,28 @@ import '../App.css';
 
 const Skills = () => {
   return (
-    <>
-       <div>
-      <h1>Skills</h1>
-        <p>Here are my skills</p>
+      <div>
+        <h1>Skills</h1>
+      <>
+    <div className='parent-div'>
+      <div className="text-block">
+              <ul>
+              <li>JavaScript</li>
+              <li>HTML</li>
+              <li>CSS</li>
+              <li>React.js</li>
+        
+              </ul>
+        </div>
+      </div>
+      
+      <Footer />
+
+      
+      </>
     </div>
       
-      <div className='footer-div-on-pages'>
-      <Footer />
-      </div>
-      </>
+      
    
     )
   };
